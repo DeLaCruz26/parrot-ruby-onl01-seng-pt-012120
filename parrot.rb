9af9620c45c36ce6fb01adab2033f0734f2b0676
Dametrik Fick <dametrik.fick@gmail.com>
2020-01-24 06:12:21 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,7 @@
-# Create method `parrot` that outputs a given phrase and
-# returns the phrase
+def parrot
+  puts "Squawk!"
+  return "Squawk!"
+  phrase = "Pretty bird!"
+  puts "#{phrase}"
+  return "#{phrase}"
+end
